346ffff9a81ee7b4f98dd6a0b7761d62514870d4
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-10-02 21:55:03 +0100

    options to set feild types for "example sentences" and "sentence translation"

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,104 +1,109 @@
 var fetchOptions = function (selectId, url, action, params = {})
 {
-	var selectEl = document.getElementById(selectId);
+    var selectEl = document.getElementById(selectId);
 
-	return new Promise((resolve, reject) =>
-	{
-		chrome.storage.local.get(selectId, (stored) =>
-		{
+    return new Promise((resolve, reject) =>
+    {
+        chrome.storage.local.get(selectId, (stored) =>
+        {
 
-			var storedVal = stored[selectId] || 'Basic';
+            var storedVal = stored[selectId] || 'Basic';
 
-			fetch(url, { method: "POST", body: JSON.stringify({ "action": action, "params": params }) }).then(r => r.json()).then(data =>
-			{
-				data.forEach((item) =>
-				{
-					e = document.createElement("option");
-					e.value = item;
-					e.text = item;
-					if (item === storedVal) e.selected = true;
-					selectEl.appendChild(e);
-				})
-				if (action === "modelFieldNames")
-				{
-					e = document.createElement("option");
-					e.value = "";
-					e.text = "";
-					selectEl.add(e);
-				}
+            fetch(url, { method: "POST", body: JSON.stringify({ "action": action, "params": params }) }).then(r => r.json()).then(data =>
+            {
+                data.forEach((item) =>
+                {
+                    e = document.createElement("option");
+                    e.value = item;
+                    e.text = item;
+                    if (item === storedVal) e.selected = true;
+                    selectEl.appendChild(e);
+                })
+                if (action === "modelFieldNames")
+                {
+                    e = document.createElement("option");
+                    e.value = "";
+                    e.text = "";
+                    selectEl.add(e);
+                }
 
-			}).then(r => resolve(r)).catch(e => console.log(e));
-		});
-	});
+            }).then(r => resolve(r)).catch(e => console.log(e));
+        });
+    });
 }
 
 var saveOption = function (selectId)
 {
-	var selectEl = document.getElementById(selectId);
-	return chrome.storage.local.set({ [selectId]: selectEl.value })
+    var selectEl = document.getElementById(selectId);
+    return chrome.storage.local.set({ [selectId]: selectEl.value })
 }
 
 document.addEventListener("DOMContentLoaded", function ()
 {
-	var urlEl = document.getElementById('ankiConnectUrl');
-	var model_Name = document.getElementById('ankiNoteNameSelected');
-	var submit_button = document.getElementById('saveAnkiBtn');
+    var urlEl = document.getElementById('ankiConnectUrl');
+    var model_Name = document.getElementById('ankiNoteNameSelected');
+    var submit_button = document.getElementById('saveAnkiBtn');
 
-	chrome.storage.local.get('ankiConnectUrl', ({ ankiConnectUrl }) =>
-	{
-		var url = ankiConnectUrl || 'http://localhost:8765';
-		urlEl.classList.add('focused');
-		urlEl.value = url;
+    chrome.storage.local.get('ankiConnectUrl', ({ ankiConnectUrl }) =>
+    {
+        var url = ankiConnectUrl || 'http://localhost:8765';
+        urlEl.classList.add('focused');
+        urlEl.value = url;
 
-		Promise.all([
-			/* Get All Deck names and all Note Types */
-			fetchOptions('ankiDeckNameSelected', url, 'deckNames'),
-			fetchOptions('ankiNoteNameSelected', url, 'modelNames') /* note type */
-		]).then(() =>
-		{
+        Promise.all([
+            /* Get All Deck names and all Note Types */
+            fetchOptions('ankiDeckNameSelected', url, 'deckNames'),
+            fetchOptions('ankiNoteNameSelected', url, 'modelNames') /* note type */
+        ]).then(() =>
+        {
 
-			/* Then we get all the Field's for the selected Note type */
-			/*      dont change 'modelFieldNames' - this is for ankiconnect */
-			var model_Name_value = model_Name.value;
-			console.log("model_Name_value = ", model_Name_value)
-			fetchOptions('ankiFieldWord', url, 'modelFieldNames', { "modelName": model_Name_value })
-			fetchOptions('ankiSentence', url, 'modelFieldNames', { "modelName": model_Name_value })
-			fetchOptions('ankiBasicTranslation', url, 'modelFieldNames', { "modelName": model_Name_value })
-			fetchOptions('ankiExtraTranslation', url, 'modelFieldNames', { "modelName": model_Name_value })
+            /* Then we get all the Field's for the selected Note type */
+            /*      dont change 'modelFieldNames' - this is for ankiconnect */
+            var model_Name_value = model_Name.value;
+            console.log("model_Name_value = ", model_Name_value)
+            fetchOptions('ankiFieldWord', url, 'modelFieldNames', { "modelName": model_Name_value })
+            fetchOptions('ankiSentence', url, 'modelFieldNames', { "modelName": model_Name_value })
+            fetchOptions('ankiSentenceTranslation', url, 'modelFieldNames', { "modelName": model_Name_value })
+            fetchOptions('ankiExampleSentence', url, 'modelFieldNames', { "modelName": model_Name_value })
+            fetchOptions('ankiBasicTranslation', url, 'modelFieldNames', { "modelName": model_Name_value })
+            fetchOptions('ankiExtraTranslation', url, 'modelFieldNames', { "modelName": model_Name_value })
 
-			model_Name.addEventListener("change", function ()
-			{
+            model_Name.addEventListener("change", function ()
+            {
+                var array = [
+                    "ankiFieldWord",
+                    "ankiSentence",
+                    "ankiSentenceTranslation",
+                    "ankiExampleSentence",
+                    "ankiBasicTranslation",
+                    "ankiExtraTranslation"
+                ];
 
-				var array = [
-					"ankiFieldWord",
-					"ankiSentence",
-					"ankiBasicTranslation",
-					"ankiExtraTranslation"
-				];
+                array.forEach((item) =>
+                {
+                    document.getElementById(item).length = 0;
+                    fetchOptions(item, url, 'modelFieldNames', { "modelName": model_Name.value });
+                })
+            })
+            // saveAnkiBtn.classList.disabled = true;
+            submit_button.addEventListener('click', (e) =>
+            {
+                Promise.all([
+                    saveOption('ankiConnectUrl'),
 
-				array.forEach((item) =>
-				{
-					document.getElementById(item).length = 0;
-					fetchOptions(item, url, 'modelFieldNames', { "modelName": model_Name.value });
-				})
-			})
-			// saveAnkiBtn.classList.disabled = true;
-			submit_button.addEventListener('click', (e) =>
-			{
-				Promise.all([
-					saveOption('ankiConnectUrl'),
+                    saveOption('ankiDeckNameSelected'),
+                    saveOption('ankiNoteNameSelected'),
 
-					saveOption('ankiDeckNameSelected'),
-					saveOption('ankiNoteNameSelected'),
-
-					saveOption('ankiFieldWord'),
-					saveOption('ankiSentence'),
-					saveOption('ankiBasicTranslation'),
-					saveOption('ankiExtraTranslation')
-				])
-					.then(() => alert(`Options saved!`))
-					.catch(error => alert(`Cannot save options: ${error}`))
-			});
-		}).catch(error => alert(`Cannot fetch options via AnkiConnect (Check Anki is started): ${error}`))
-	});
+                    saveOption('ankiFieldWord'),
+                    saveOption('ankiSentence'),
+                    saveOption('ankiSentenceTranslation'),
+                    saveOption('ankiExampleSentence'),
+                    saveOption('ankiBasicTranslation'),
+                    saveOption('ankiExtraTranslation')
+                ])
+                    .then(() => alert(`Options saved!`))
+                    .catch(error => alert(`Cannot save options: ${error}`))
+            });
+        }).catch(error => alert(`Cannot fetch options via AnkiConnect (Check Anki is started): ${error}`))
+    });
 });
